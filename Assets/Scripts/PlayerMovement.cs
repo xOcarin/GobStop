1ce55bc50108d6ca843ab4e5c24a8cf08b227d97
xOcarin <119756949+xOcarin@users.noreply.github.com>
2023-10-04 21:20:30 -0400

    Added basic movement and player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxMoveSpeed = 7f;
+    public float jumpForce = 10f;
+    public float groundCheckRadius = 0.1f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    [SerializeField]
+    private Transform groundCheck;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // Horizontal movement
+        float horizontalInput = Input.GetAxis("Horizontal"); // Updated input axis to "Horizontal"
+        float targetSpeed = horizontalInput * maxMoveSpeed;
+        rb.velocity = new Vector2(targetSpeed, rb.velocity.y);
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    //this is just to show the grounded circle in the editor
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
